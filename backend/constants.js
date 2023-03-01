d026f0efe7afc1c226a74bceb1d6f06fd1126568
naushad2403 <naushadali2403@gmail.com>
2023-03-01 20:28:56 +0530

    - Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x72457D862bD4b1a7fb0F4582D150Ed267767b64B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
